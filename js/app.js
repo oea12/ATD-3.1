67375fdaf7a8563247c7e7a8f34ef66c674a1bd4
oea12 <66128082+oea12@users.noreply.github.com>
2022-03-22 15:07:21 -0600

    HEAD

---
+++ js/app.js
@@ -0,0 +1,144 @@
+var Alert = new CustomAlert();
+
+function CustomAlert(){
+  this.render = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal').innerHTML = '<button onclick="Alert.ok()">x</button>';
+  }
+
+this.ok = function(){
+  document.getElementById('popUpBox').style.display = "none";
+  document.getElementById('popUpOverlay').style.display = "none";
+}
+}
+var Alert2 = new CustomAlert2();
+
+function CustomAlert2(){
+  this.render2 = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox2');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal2').innerHTML = '<button onclick="Alert2.ok2()">x</button>';
+  }
+
+this.ok2 = function(){
+  document.getElementById('popUpBox2').style.display = "none";
+  document.getElementById('popUpOverlay2').style.display = "none";
+}
+}
+var Alert3 = new CustomAlert3();
+
+function CustomAlert3(){
+  this.render3 = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox3');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal3').innerHTML = '<button onclick="Alert3.ok3()">x</button>';
+  }
+
+this.ok3 = function(){
+  document.getElementById('popUpBox3').style.display = "none";
+  document.getElementById('popUpOverlay3').style.display = "none";
+}
+}
+var Alert4 = new CustomAlert4();
+
+function CustomAlert4(){
+  this.render4 = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox4');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal4').innerHTML = '<button onclick="Alert4.ok4()">x</button>';
+  }
+
+this.ok4 = function(){
+  document.getElementById('popUpBox4').style.display = "none";
+  document.getElementById('popUpOverlay4').style.display = "none";
+}
+}
+var Alert5 = new CustomAlert5();
+
+function CustomAlert5(){
+  this.render5 = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox5');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal5').innerHTML = '<button onclick="Alert5.ok5()">x</button>';
+  }
+
+this.ok5 = function(){
+  document.getElementById('popUpBox5').style.display = "none";
+  document.getElementById('popUpOverlay5').style.display = "none";
+}
+}
+var Alert6 = new CustomAlert6();
+
+function CustomAlert6(){
+  this.render6 = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox6');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal6').innerHTML = '<button onclick="Alert6.ok6()">x</button>';
+  }
+
+this.ok6 = function(){
+  document.getElementById('popUpBox6').style.display = "none";
+  document.getElementById('popUpOverlay6').style.display = "none";
+}
+}
+var Alert7 = new CustomAlert7();
+
+function CustomAlert7(){
+  this.render7 = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox7');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal7').innerHTML = '<button onclick="Alert7.ok7()">x</button>';
+  }
+
+this.ok7 = function(){
+  document.getElementById('popUpBox7').style.display = "none";
+  document.getElementById('popUpOverlay7').style.display = "none";
+}
+}
+var Alert8 = new CustomAlert8();
+
+function CustomAlert8(){
+  this.render8 = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox8');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal8').innerHTML = '<button onclick="Alert8.ok8()">x</button>';
+  }
+
+this.ok8 = function(){
+  document.getElementById('popUpBox8').style.display = "none";
+  document.getElementById('popUpOverlay8').style.display = "none";
+}
+}
+var Alert9 = new CustomAlert9();
+
+function CustomAlert9(){
+  this.render9 = function(){
+      //Show Modal
+      let popUpBox = document.getElementById('popUpBox9');
+      popUpBox.style.display = "block";
+      //Close Modal
+      document.getElementById('closeModal9').innerHTML = '<button onclick="Alert9.ok9()">x</button>';
+  }
+
+this.ok9 = function(){
+  document.getElementById('popUpBox9').style.display = "none";
+  document.getElementById('popUpOverlay9').style.display = "none";
+}
+}
